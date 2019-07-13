8a61ea9db19e463900b2cbf9034f267b8d6c4b1a
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-07-13 18:16:24 +0930

    Day 004 completed! Work with 32-bit pixel directly (+6 squashed commit)
    Squashed commit:
    
    [e5e83b4] Removed redundant cast
    
    Row is already a pointer to a unit8
    
    [5c62330] Post-increment for pixel
    
    [f05b2a1] Analysis of image result with varying bitcounts, pixel sizes and no. pxl set per X coord
    
    [9f7cd68] Analysis of movement
    
    [0acda0a] Animation of backbuffer!
    
    Day 004 WIP, 53:43
    
    [ed63325] Day 004 WIP
    
    Up to 10:19

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -7,6 +7,7 @@
    // ^ version control system keyword replacement
 
     #include <windows.h>
+    #include <stdint.h> //uint8_t
 
     // Undefine the following to forcce use of ANSI or UNICODE versions - we will always use ANSI,
     // so doing the below will: 1) make us use suffix A 2) If someone tries to compile in Unicode mode, an error will occur (good)
@@ -15,80 +16,141 @@
     #undef TranslateMessage
     #undef DispatchMessage
     #undef DefWindowProc 
+    #undef PeekMessage 
 
-    // static in C/C++ means different things depending on where it is placed...so Casey does this neat trick:
     #define internal_function static 
     #define local_persist static
-    #define global_variable static// doesn't this still have the same effect as internal_function? as in only acessed from here? We get auto init to zero though
+    #define global_variable static
+
+    // Platform (machine) independent size
+    typedef uint8_t uint8;
+    typedef uint16_t uint16;
+    typedef uint32_t uint32;
+    typedef uint64_t uint64;
+
+    typedef int8_t int8;
+    typedef int16_t int16;
+    typedef int32_t int32;
+    typedef int64_t int64;
 
     //TODO: This is a global for now
     global_variable bool Running;
     global_variable BITMAPINFO BitmapInfo; //we want global persistence
     global_variable void * BitmapMemory; 
-    global_variable HBITMAP BitmapHandle;
-    global_variable HDC BitmapDeviceContext;
+    global_variable int BitmapWidth; 
+    global_variable int BitmapHeight; 
+    global_variable int BytesPerPixel = 4;
 
-    // DIB = device independent bitmap: cf GDI
-    // Note Casey prefixes any of his own functions (non windows API) with "Win32" to specify platform specific functions
-    // and to avoid collision with any Windows functions. I am not sure if I think this is the clearest though, as it makes it look
-    // like Windows API...but the functions will usually only work for Windows anyway, so maybe its fine...
+    void
+    RenderWeirdGradient(int XOffset, int YOffset)
+    {
+        int Width = BitmapWidth;
+
+        // casting void to char type (want per byte arithmetic)
+        uint8 *Row = (uint8 *)BitmapMemory;
+        int Pitch = Width*BytesPerPixel;
+
+        for (int Y = 0; Y < BitmapHeight; ++Y)
+        {
+            uint32 *Pixel = (uint32 *)Row;
+            for (int X = 0; X < BitmapWidth; ++X)
+            {
+                // Explanation:
+                // 
+                // Memory(hex) : BB GG RR xx (xx is pad)
+                // Register (hex. Assuming little endian architecture) xx RR GG BB
+                //
+                // So now that we are dealing with 32 bits directly (i.e 32-bit pixel) we rewrite
+
+                /*
+                    *Pixel++ = X + XOffset;
+
+                    *Pixel++ = Y + YOffset;
+
+                    *Pixel++ = 0;
+
+                    *Pixel++ = 0;
+                */
+
+                // as
+                uint8 Blue = X + XOffset;
+                uint8 Green = Y + YOffset;
+                uint8 Red = 0;
+                uint8 Pad = 0;
+
+                *Pixel++ = (Blue | Green << 8 | Red << 16 | Pad << 24);
+                // TODO : operator precedence : remove brackets
+            }
+
+            Row += Pitch;
+            // TODO Casey makes a comment here that the separation of operations (Row adding and Pixel)
+            // may seem inefficient, we do ++Pixel because sometimes byte boundaries may differ(??).
+            // Its < 33:30
+        }
+
+    }
+
+
     void
     Win32ResizeDIBSection(int Width , int Height)
     {
-        if(BitmapHandle)
+
+        if (BitmapMemory)
         {
-            DeleteObject(BitmapHandle);
-        }
-        if(!BitmapDeviceContext) 
-        {
-            BitmapDeviceContext = CreateCompatibleDC(0);
+            VirtualFree(BitmapMemory, 0, MEM_RELEASE);
         }
 
-        BITMAPINFO BitmapInfo;
+        BitmapWidth = Width;
+        BitmapHeight = Height;
+
         // Specify our BitmapInfo structure attributes
         BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-        BitmapInfo.bmiHeader.biWidth = Width;
-        BitmapInfo.bmiHeader.biHeight = Height;
+        BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+        
+        // Top down so negative (see MSDN)
+        BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
         BitmapInfo.bmiHeader.biPlanes = 1;
-        BitmapInfo.bmiHeader.biBitCount = 32;
+        
+        // 32 for 4byte alignment (24  =3 bytes; 1 more byte for this "padding" to give 
+        // alignment on 4 byte boundary (x86 architecture efficiency)
+		BitmapInfo.bmiHeader.biBitCount = 32;
         BitmapInfo.bmiHeader.biCompression = BI_RGB;
-        // Casey :Since BitmapInfo is static, its initialised to zero,
-        // so we don't need to set the rest of the fields to 0...
-        // *BUT* I do not think this is correct = the bmiHeader field is not necessarily all field zero (usually we will, but this
-        // is not guaranteed)
         BitmapInfo.bmiHeader.biSizeImage = 0;
         BitmapInfo.bmiHeader.biXPelsPerMeter = 0;
         BitmapInfo.bmiHeader.biYPelsPerMeter = 0;
         BitmapInfo.bmiHeader.biClrUsed = 0;
         BitmapInfo.bmiHeader.biClrImportant = 0;
 
+        int BitmapMemorySize = (Width*Height)*BytesPerPixel;
 
-        BitmapHandle = CreateDIBSection(BitmapDeviceContext,
-                                       &BitmapInfo,
-                                       DIB_RGB_COLORS,
-                                       &BitmapMemory,
-                                       NULL,0);
+        // VirtualAlloc returns pages - a bit more "raw" than HeapAlloc
+        BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 
-            
-        
+        //TODO : we might want to initialise this to black 
+
+
     }
 
     void
-    Win32UpdateWindow(HDC DeviceContext, int X , int Y, int Width, int Height)
+    Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X , int Y, int Width, int Height)
     {
+        int WindowWidth = WindowRect->right - WindowRect->left;
+        int WindowHeight = WindowRect->bottom - WindowRect->top;
         StretchDIBits(DeviceContext,
+                      
+                      /* Draw to whole window FIRST, leave this (subsection of window
+                       * to later...
                       X, Y, Width, Height,
                       X, Y, Width, Height,
+                      */
+                      // 0,0 to start at top left corner I believe
+                      0, 0, BitmapWidth, BitmapHeight,
+                      0, 0, WindowWidth, WindowHeight,
                       BitmapMemory,
                       &BitmapInfo,
                       DIB_RGB_COLORS,
                       SRCCOPY); 
-
-
     }
-
-
-
 
     LRESULT CALLBACK
     MainWindowCallback(HWND WindowHandle,
@@ -105,7 +167,9 @@
                 RECT ClientRect;
                 GetClientRect(WindowHandle, &ClientRect);
                 int Width = ClientRect.right - ClientRect.left;
-                int Height = ClientRect.top - ClientRect.bottom;
+                
+                // We look from the top of the screen! (think how an image is rendered usually - from l to r, t ot b)
+                int Height = ClientRect.bottom - ClientRect.top;
                 Win32ResizeDIBSection(Width, Height);
             } break;
 
@@ -147,12 +211,13 @@
                 HDC DeviceContext = BeginPaint(WindowHandle, &Paint);
                 LONG X = Paint.rcPaint.left;
                 LONG Y = Paint.rcPaint.right;
-                // I had width zero, what does this do?
                 LONG Width = Paint.rcPaint.right - Paint.rcPaint.left;
                 LONG Height = Paint.rcPaint.top - Paint.rcPaint.bottom;
-                Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
-                // Don't do willy-nilly - Casey only uses static for debugging, where it can be useful
-                // Also understand what he is saying with lexical scope and static vs global
+
+                RECT ClientRect;
+                GetClientRect(WindowHandle, &ClientRect);
+                Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
+
                 local_persist DWORD Operation = BLACKNESS;
                 PatBlt(DeviceContext, X, Y, Width, Height, Operation);
                 switch(Operation)
@@ -175,8 +240,7 @@
                     }
                 }
 
-
-                SetPixel(DeviceContext, 100, 100, RGB(255,0,0));
+                //SetPixel(DeviceContext, 100, 100, RGB(255,0,0));
                 EndPaint(WindowHandle, &Paint);
             } break;
 
@@ -190,8 +254,7 @@
     }
 
 
-    int CALLBACK // #define CALLBACK/WINAPI/APIENTRY __stdcall so we could (and I think should in this case) use WINAPI or APIENTRY - CALLBACK may be a
-                // a misleading marker
+    int CALLBACK 
     WinMain(HINSTANCE Instance,
            HINSTANCE PrevInstance,
            LPSTR CmdLine,
@@ -199,23 +262,12 @@
     {
         int * IntPtr = 0;
         
-        WNDCLASS WindowClass = {}; //set all to zero - C++; C may need {0}
+        WNDCLASS WindowClass = {}; 
 
-        // Don't need WindowsClass.style _at all_  apparently Casey (or I can if I want) check
-        WindowClass.style = CS_OWNDC;//|CS_HREDRAW|CS_VREDRAW; //MSDN for more detail
+        WindowClass.style = CS_OWNDC;
 
-        // Casey: We need to know the callback name here, because as we saw around 30min at Day 2 Q&A, windows can ask for it
-        // at anytime (before _we_ dispatch it). Like Casey says, would be better if we didn't need to define the WindowProc callback
-        // ourselves.
-        // 
-        // Remove it and see - you will get a runtime error, as windows wants to call it.
-        //
-        //BUT check this out! I decided to set it to DefWindowProc; And we are back in business! BUT ofcourse we are then not 
-        //able to handle messages. We could still enter the default case in our implementation of the callback and print out the message
-        //and maybe even map it to the name using a table we can make for eg. the WINE website with windows messages and their values.
         WindowClass.lpfnWndProc = MainWindowCallback;
-        //WindowClass.lpfnWndProc = DefWindowProc;
-        WindowClass.hInstance = Instance; // could call kernel function GetModuleHandle(0)
+        WindowClass.hInstance = Instance; 
         WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
         if(RegisterClassA(&WindowClass))
@@ -235,20 +287,35 @@
                                   0);
             if(WindowHandle)
             {
+                int XOffset = 0;
+                int YOffset = 0;
                 MSG Message;
                 Running = true;
                 while(Running)
                 {
-                    BOOL MessageResult = GetMessageA(&Message,0, 0, 0);
-                    if(MessageResult > 0)
+                    while (PeekMessageA(&Message,0,0,0,PM_REMOVE))
                     {
+                        if (Message.message == WM_QUIT)
+                        {
+                            Running = false;
+                        }
+                            
                         TranslateMessage(&Message);
                         DispatchMessageA(&Message);
                     }
-                    else
-                    {
-                        break;
-                    }
+
+                    RenderWeirdGradient(XOffset, YOffset);
+
+                    HDC DeviceContext = GetDC(WindowHandle);
+                    RECT ClientRect;
+                    GetClientRect(WindowHandle, &ClientRect);
+                    int WindowWidth = ClientRect.right - ClientRect.left;
+                    int WindowHeight = ClientRect.bottom - ClientRect.top;
+                    Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+                    ReleaseDC(WindowHandle, DeviceContext);
+
+                    ++XOffset;
+
                 }
             }
         }
@@ -260,5 +327,3 @@
         return(0);
     }
 
-
-       
